4be5ae3fa6b2da5b5f7baba8f6fa4b1378b1e5b9
meer Afzal <do4best@gmail.com>
2024-06-12 18:29:46 +0500

    The hidden search projecte errors were fixed.

--- hiddenSearch/hiddenSearch.jsx
+++ hiddenSearch/hiddenSearch.jsx
@@ -2,52 +2,69 @@ import React, {useEffect, useRef, useState} from 'react';
 import {BsSearch} from "react-icons/bs";
 
 function HiddenSearch(props) {
+    {/*  This is use state object*/}
     const [uiPros,setUiPros] = useState({
         bg:"purple",
         shadow:"",
         transition:"all .3s ease",
         opacity:0,
         showSearchIcon:true,
-        shadowSearchBar:false
+        shadowSearchBar:true,
+        borderBottomColor:"#fff"
     })
-
+    let body = document.body.style
+    {/*  This is search bar style */}
     let bsSearchStyle={
-        fontsize:50,
+        color: "#fff",
+
+        fontsize:100,
         cursor:"pointer",
         position:"absolute",
         top:20,
         right:20,
-        padding:"1rem .3rem",
-        border:"none",
-        outline:"none",
-        background:"transparent",
-        borderBottom:"1px solid #333",
-        fontSize:"1.3rem",
-        color: "#eee",
-        boxShadow:"0px 55px 60px -15px rgba(0,0,0,.75)",
-        opacity:uiPros.opacity,
-        transition:"all .3s ease"
+        // padding:"1rem .3rem",
+        // border:"none    ",
+        // outline:"none",
+        // background:"transparent",
+        // borderBottom:`1px solid ${uiPros.borderBottomColor}`,
+        // fontSize:"1.3rem",
+        //
+        // boxShadow:"0px 55px 60px -15px rgba(0,0,0,.75)",
+        // opacity:uiPros.opacity,
+        // transition:"all .3s ease"
     }
+    {/*  This is input style */}
     let inputStyle={
         color:"#fff",
- margin:"10vh",
+        margin:"10vh 25vw",
         height:"30px",
-        width:"20vh"
+        width:"20vh",
+        padding: "1rem .3rem",
+        border: "none",
+        outline: "none",
+        background:"transparent",
+        borderBottom: "1px solid #333",
+        fontSize: "1.3rem",
+        boxShadow:"0px 55px 60px -15px rgba(0,0,0,0.75)",
+        opacity: uiPros.opacity,
+        transition:"all .3s ease"
     }
+    {/*  This is use effect it is called when an impact is created*/}
     useEffect(() => {
-        document.body.style.background = uiPros.bg;
-        document.body.style.boxShadow = uiPros.shadow;
-        document.body.style.transition = uiPros.transition;
+        body.background = uiPros.bg;
+        body.boxShadow = uiPros.shadow;
+        body.transition = uiPros.transition;
 
     }, [uiPros.shadow]);
     const showSearch = ()  =>{
         setUiPros({
             opacity: 1,
-            showSearch: false
+            showSearchIcon: false
         })
     }
+
     const inputEle = useRef(null)
-    const handelSearchBlur=(e)=>{
+    const handelSearchBlur=()=>{
         setUiPros({
             shadow: "none",
             opacity: 0,
@@ -60,18 +77,19 @@ function HiddenSearch(props) {
             shadow: "insect 0 -60vh 30vw 200px rgba(0,0,0,0.8)",
             borderBottomColor:"green",
 
+
         })
         inputEle.current.focus()
     }
 
     return (
         <>
-        <div className="container" style={{height:"100vh"}}>
+            <div className="container" style={{height:"100vh"}}>
 
-            {uiPros.showSearchIcon ? (<BsSearch style={bsSearchStyle} onClick={showSearch}/>) :
-                <input type="text" placeholder={"search"} style={inputStyle} onFocus={handelSearchFocus} ref={inputEle}
-                       onBlur={handelSearchBlur}/>}
-        </div>
+                {uiPros.showSearchIcon ? (<BsSearch style={bsSearchStyle} onClick={showSearch}/>) :
+                    <input type="text" placeholder={"search"} style={inputStyle} onFocus={handelSearchFocus} ref={inputEle}
+                           onBlur={handelSearchBlur}/>}
+            </div>
 
         </>
     );
