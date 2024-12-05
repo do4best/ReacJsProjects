8f25f10fad06afb249bacf61f1d93825d551e901
Meer Afzal <do4best@gmail.com>
2024-12-05 18:39:07 +0500

    display error still presist in topPics

---
+++ src/components/yumproject/data1.js
@@ -0,0 +1,34 @@
+import kabab from './kabab.jpg'
+import pizza from './pizza.jpg'
+import burger from './burger.jpg'
+import buryani from './baryani.jpg'
+export const topPicks=[
+    {
+        id:1,
+        title:'Kabab',
+        price:"500Rs",
+        img: {kabab}
+        
+    },
+    {
+        id:2,
+        title:'Pizza',
+        price:"1500Rs",
+        img: {pizza}
+
+    },
+    {
+        id:3,
+        title:'Burger',
+        price:"150Rs",
+        img: {burger}
+
+    },
+    {
+        id:4,
+        title:'Baryani',
+        price:"750Rs",
+        img: {buryani}
+
+    },
+]
--- src/components/yumproject/topPics.jsx
+++ src/components/yumproject/topPics.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {topPicks} from "./data1.js";
 
-function TopPics(props) {
+function TopPics() {
     return (
-        <></>
+        <>
+        <h1 className={"text-orange-500 font-bold text-2xl text-center py-2 "}>Top Pics</h1>
+            <div className="hidden lg:flex max-[1520px] m-auto py-2 px-2">
+                {
+                    topPicks.map((id)=>{
+                        return (
+                            <div className={"rounded-3xl relative"} >
+                                <div className={"absolute w-full h-full bg-black/50 rounded-3xl text-white"}>
+                                    <p className={"px-2"}>{id.title}</p>
+                                </div>
+
+                                <img className={"h-[200px] w-full"} src={id.img} alt={id.title}/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     );
 }
 
